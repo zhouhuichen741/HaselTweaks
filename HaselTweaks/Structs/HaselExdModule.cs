8d95850eba4220d56fdf26f8bcd401b82bab386e
Haselnussbomber <mail@haselnussbomber.de>
2026-04-29 17:10:04 +0200

    Allow restoring outfits

---
+++ HaselTweaks/Structs/HaselExdModule.cs
@@ -0,0 +1,8 @@
+namespace HaselTweaks.Structs;
+
+[GenerateInterop]
+public partial struct HaselExdModule
+{
+    [MemberFunction("E8 ?? ?? ?? ?? 33 F6 4C 8B D0")]
+    public static partial nint GetMirageStoreSetItemRowById(uint rowId);
+}
--- HaselTweaks/Windows/GlamourDresserArmoireAlertWindow.cs
+++ HaselTweaks/Windows/GlamourDresserArmoireAlertWindow.cs
@@ -93,7 +93,19 @@ public unsafe partial class GlamourDresserArmoireAlertWindow : SimpleWindow
 
             if (ImGui.IsItemHovered())
             {
-                ExdModule.GetItemRowById(item); // make sure item is loaded, otherwise RestorePrismBoxItem will fail
+                // make sure items are loaded, otherwise restoring will fail
+
+                if (_excelService.TryGetRow<MirageStoreSetItem>(item, out var set))
+                {
+                    HaselExdModule.GetMirageStoreSetItemRowById(item);
+
+                    foreach (var setItem in set.Items.Where(item => item.RowId != 0))
+                        ExdModule.GetItemRowById(setItem.RowId);
+                }
+                else
+                {
+                    ExdModule.GetItemRowById(item);
+                }
             }
 
             ImCursor.Position = pos + new Vector2(
@@ -117,14 +129,45 @@ public unsafe partial class GlamourDresserArmoireAlertWindow : SimpleWindow
     private void RestoreItem(ItemHandle item)
     {
         var mirageManager = MirageManager.Instance();
+        if (!mirageManager->PrismBoxRequested)
+            return;
         if (!mirageManager->PrismBoxLoaded)
             return;
 
-        var itemIndex = mirageManager->PrismBoxItemIds.IndexOf(item);
+        var itemId = item.BaseItemId;
+        var itemIndex = mirageManager->PrismBoxItemIds.IndexOf(itemId);
         if (itemIndex == -1)
             return;
 
-        _logger.LogDebug("Restoring item {index}", itemIndex);
-        IsUpdatePending = mirageManager->RestorePrismBoxItem((uint)itemIndex);
+        if (_excelService.TryGetRow<MirageStoreSetItem>(itemId, out var lookupRow))
+        {
+            // stain ids store bits of which slots are locked.
+            // we invert that to know which slots are stored in the glamour dresser.
+            // then we mask the value by which slots/bits actually have items in the sheet
+
+            var bits = stackalloc byte[2];
+            bits[0] = (byte)~mirageManager->PrismBoxStain0Ids[itemIndex];
+            bits[1] = (byte)~mirageManager->PrismBoxStain1Ids[itemIndex];
+
+            ushort mask = 0;
+
+            for (var i = 0; i < lookupRow.Items.Count; i++)
+            {
+                if (lookupRow.Items[i].RowId != 0)
+                {
+                    mask |= (ushort)(1 << i);
+                }
+            }
+
+            *(ushort*)bits &= mask;
+
+            _logger.LogDebug("Restoring set items {index} with bits {b1} | {b2}", itemIndex, Convert.ToString(bits[0], 2), Convert.ToString(bits[1], 2));
+            IsUpdatePending = mirageManager->RestorePrismBoxSetItem((uint)itemIndex, bits);
+        }
+        else
+        {
+            _logger.LogDebug("Restoring item {index}", itemIndex);
+            IsUpdatePending = mirageManager->RestorePrismBoxItem((uint)itemIndex);
+        }
     }
 }
